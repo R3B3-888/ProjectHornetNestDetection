cc72009ea36ac1bd2a23a6fb0733d1a8b49bc4b3
Alexis <alexis.hoffmann@etu.u-bordeaux.fr>
2022-01-03 09:27:05 +0100

    Cleaned TreeWithNest

--- Assets/Scripts/TreeWithNest.cs
+++ Assets/Scripts/TreeWithNest.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class TreeWithNest
 {
     #region Variables
-         
     private int id;
     private GameObject nest;
     private GameObject tree;
@@ -13,17 +12,11 @@ public class TreeWithNest
     private float y;
     private float z;
     private float depthOffset = 1.0f;
-
     #endregion
-    
+
     #region Getter And Setter
     public int Id { get => id; set => id = value; }
     public GameObject Nest { get => nest; set => nest = value; }
-    public GameObject Tree { get => tree; set => tree = value; }
-    public Vector3 TreeSize { get => treeSize; set => treeSize = value; }
-    public float X { get => x; set => x = value; }
-    public float Y { get => y; set => y = value; }
-    public float Z { get => z; set => z = value; }
     #endregion
 
     public TreeWithNest(int id, GameObject tree, GameObject nest)
@@ -31,17 +24,17 @@ public class TreeWithNest
         this.id = id;
         this.nest = nest;
         nest.gameObject.name = "Nest";
-        treeSize =  tree.GetComponent<MeshRenderer>().bounds.size;
+        treeSize = tree.GetComponent<MeshRenderer>().bounds.size;
         update(tree);
     }
 
-    public void update(GameObject tree) 
+    public void update(GameObject tree)
     {
         this.tree = tree;
         x = tree.transform.position.x;
         y = tree.transform.position.y;
         z = tree.transform.position.z;
-        nest.transform.position = new Vector3(x, y + treeSize.y * 5/6 * depthOffset, z);
+        nest.transform.position = new Vector3(x, y + treeSize.y * 5 / 6 * depthOffset, z);
         // nest.transform.position = new Vector3(50, 9, 10);
     }
 
